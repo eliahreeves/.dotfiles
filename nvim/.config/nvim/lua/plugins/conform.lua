ae1582f29749e5d7ceebe00326e24378fbd9f662
Eliah Reeves <Eliah.Reeves@water.ca.gov>
2025-04-03 17:36:27 -0700

    fixed keybind

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -8,7 +8,7 @@ return {
             function()
                 require("conform").format({ async = true })
             end,
-            mode = "",
+            mode = "n",
             desc = "Format buffer",
         },
     },
