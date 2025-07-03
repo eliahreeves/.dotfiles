a2b1ed52b44331686a8ad559181c33eaa8341c44
Eliah Reeves <ereeclimb@gmail.com>
2025-07-02 18:22:44 -0700

    hyprsunset + ghostty

---
+++ waybar/.config/waybar/scripts/bluelight_toggle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+STATUS_FILE="/tmp/bluelight.status"
+
+# check current status
+if [ ! -f "$STATUS_FILE" ]; then
+	echo " "
+	exit
+fi
+
+status=$(cat "$STATUS_FILE")
+
+if [ "$status" == " " ]; then
+	echo " "
+else
+	echo " "
+fi
---
+++ waybar/.config/waybar/scripts/bluelight_toggle_action.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+STATUS_FILE="/tmp/bluelight.status"
+
+if [ -f "$STATUS_FILE" ] && [ "$(cat $STATUS_FILE)" == " " ]; then
+	# turn it off
+	hyprctl hyprsunset temperature identity >/dev/null
+	echo " " >"$STATUS_FILE"
+else
+	# turn it on
+	hyprctl hyprsunset temperature 2600 >/dev/null
+	echo " " >"$STATUS_FILE"
+fi
+
+pkill -SIGRTMIN+10 waybar
