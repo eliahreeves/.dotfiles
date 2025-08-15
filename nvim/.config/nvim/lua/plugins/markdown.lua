e0345c04409bb6c1da63bcf672df796124a7e662
Eliah Reeves <ereeclimb@gmail.com>
2025-08-14 18:09:22 -0700

    multi-cursor fix

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+        vim.g.mkdp_auto_close = 1
+        
+        -- Custom function to open Firefox
+        vim.cmd([[
+            function! OpenMarkdownPreview(url)
+                execute "silent !firefox " . a:url . " &"
+            endfunction
+        ]])
+        
+        vim.g.mkdp_browserfunc = 'OpenMarkdownPreview'
+    end,
+    ft = { "markdown" },
+}
--- nvim/.config/nvim/lua/plugins/multi-cursor.lua
+++ nvim/.config/nvim/lua/plugins/multi-cursor.lua
@@ -10,6 +10,7 @@ return {
         { "<C-LeftDrag>", mode = "n" },
         { "<C-LeftRelease>", mode = "n" },
         { "<Leader>uc", mode = { "n", "x" } },
+        { "<Leader>n", mode = { "n", "x" } },
     },
     config = function()
         local mc = require("multicursor-nvim")
@@ -29,6 +30,18 @@ return {
         set({ "n", "x" }, "<Leader><Down>", function()
             mc.lineSkipCursor(1)
         end)
+        set({ "n", "x" }, "<Leader>n", function()
+            mc.matchAddCursor(1)
+        end)
+        -- set({ "n", "x" }, "<Leader>s", function()
+        --     mc.matchSkipCursor(1)
+        -- end)
+        set({ "n", "x" }, "<Leader>N", function()
+            mc.matchAddCursor(-1)
+        end)
+        -- set({ "n", "x" }, "<Leader>S", function()
+        --     mc.matchSkipCursor(-1)
+        -- end)
 
         set("n", "<C-LeftMouse>", mc.handleMouse)
         set("n", "<C-LeftDrag>", mc.handleMouseDrag)
