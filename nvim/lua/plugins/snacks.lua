978b81e77506d123648d49aa33bf55274d1ebe28
Eliah Reeves <ereeclimb@gmail.com>
2026-01-06 12:34:08 -0800

    niri

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -97,7 +97,7 @@ return {
         {
             "<leader><space>",
             function()
-                Snacks.picker.smart()
+                Snacks.picker.files()
             end,
             desc = "Smart Find Files",
         },
